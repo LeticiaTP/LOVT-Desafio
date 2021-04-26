215d790d8fd472ace0ec4cb861f87878587a4e3f
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-04-26 13:33:24 -0300

    Add dark mode

--- js/main.js
+++ js/main.js
@@ -1,17 +1,10 @@
-// console.log('JS externo funcionou uhuuu!');
-// console.log(window);
+const darkMode = () => {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
 
-// alert('Bem vindo(a)!') //mensagem para usuário
+    let footer = document.querySelector('footer');
+    footer.classList.toggle("footer-darkmode")
 
-// let adulto = confirm('Você tem mais de 18 anos?'); //retorna booleano
-
-// let nomeUsuario = prompt('Qual seu nome?'); //retorna texto inserido pelo usuário
-// console.log(nomeUsuario);
-
-// let elementHeader = document.querySelector('header .saudacao'); //seleciona o elemento html
-// elementHeader.innerText = `Olá, ${nomeUsuario}!`; //atribui conteúdo ao elemento selecionado
-
-// let logo = document.getElementById('logo');
-// console.log(logo);
-
-// logo.style.color = "orange";
+    let header = document.querySelector('header');
+    header.classList.toggle("header-darkmode")
+}
