86c077c0754db3278f5e40c853b680fe422f403a
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-04-26 11:16:25 -0300

    Style das ofertas pronto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('JS externo funcionou uhuuu!')
+// console.log('JS externo funcionou uhuuu!');
+// console.log(window);
+
+// alert('Bem vindo(a)!') //mensagem para usuário
+
+// let adulto = confirm('Você tem mais de 18 anos?'); //retorna booleano
+
+// let nomeUsuario = prompt('Qual seu nome?'); //retorna texto inserido pelo usuário
+// console.log(nomeUsuario);
+
+// let elementHeader = document.querySelector('header .saudacao'); //seleciona o elemento html
+// elementHeader.innerText = `Olá, ${nomeUsuario}!`; //atribui conteúdo ao elemento selecionado
+
+// let logo = document.getElementById('logo-img');
+// console.log(logo);
+
+// logo.style.color = "orange";
