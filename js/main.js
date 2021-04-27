02f0a835b63af36bc1dc19df3a49bd10c83e1636
Letícia Paulino <karlaleticiatavares@gmail.com>
2021-04-27 15:06:37 -0300

    Formulário de contato pronto (HTML + CSS + JS)

--- js/main.js
+++ js/main.js
@@ -8,3 +8,40 @@ const darkMode = () => {
     let header = document.querySelector('header');
     header.classList.toggle("header-darkmode")
 }
+
+let formContact = document.querySelector("#form_contact");
+formContact.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    
+    let nomeValue = document.querySelector("#input_nome").value;
+    let emailValue = document.querySelector("#input_email").value;
+    let phoneValue = document.querySelector("#input_telefone").value;
+    let mensagemValue = document.querySelector("#mensagem").value;
+
+    const emailDefault = /^[a-zA-Z0-9][a-zA-Z0-9\._-]+@([a-zA-Z0-9\._-]+\.)[a-zA-Z-0-9]{2,3}$/;
+
+    if (nomeValue && emailValue && phoneValue && mensagemValue) {
+
+        if (nomeValue.length >= 2) {
+
+            if (phoneValue.length >= 8) {
+
+                if (emailDefault.test(emailValue)) {
+                    formContact.submit();
+                } else {
+                    error.innerHTML = '<strong> Correct the email! </strong>';
+                }
+
+            }else {
+                error.innerHTML = '<strong> The phone number need to be 8 numbers! </strong>';
+            }
+
+        } else {
+            error.innerHTML = '<strong> The name need to be more than 2 letters! </strong>';
+        }
+
+    } else {
+        error.innerHTML = '<strong> All the fields need to be completed! </strong>';
+    }
+
+});
